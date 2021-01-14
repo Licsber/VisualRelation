66393e84eb33dbdcdd4366c834b23f08cf443237
Jiale  Liu <licsber@njit.edu.cn>
2021-01-14 16:55:48 +0800

    盲猜config参数

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import os
 
 
 def main():
-
-    base_model.train_test()
+    config = {
+        'num_batch': 128,
+        'num_ways': 32,
+        'num_shots': 32,
+        'num_query': 1,
+        'feature_dim': 32,
+        'num_layers': 32,
+        'in_feature': 32,
+        'out_feature': 32,
+        'class_num': 32,
+    }
+    
+    base_model.train_test(config)
 
 if __name__ == "__main__":
     main()
