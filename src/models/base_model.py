a6b7f157df84646593216f1d355086f04a5a8384
Jiale  Liu <licsber@njit.edu.cn>
2021-01-14 16:44:00 +0800

    修改相对路径引用

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -7,7 +7,7 @@ from torch.optim.lr_scheduler import MultiStepLR
 import yaml
 import sys
 
-import DualGraphMoudle as DualGraphModule
+import src.models.DualGraphMoudle as DualGraphModule
 from src.utils.test_utils import VRDTrainTester
 from torch.nn import functional as F
 
